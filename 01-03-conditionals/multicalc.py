854139de7a05ccce26233ae9cef8e92732d8b3c4
Jess3331085 <jess333.px@gmail.com>
2024-07-31 14:36:23 +0700

    multicalc

---
+++ 01-03-conditionals/multicalc.py
@@ -0,0 +1,36 @@
+# 1. The app shows welcome display
+print("selamat datang!")
+print("")
+print("""1. addition
+2.subtraction
+3.multiplication
+4.division 
+      """)
+
+# 2. The app asks for user's option
+option = input("choose your option")
+print(option)
+
+number1 = input ("insert first number")
+number2 = input ("insert second number")
+hasil = 0
+
+# 3. if user want addition,do addition
+if option == "1":
+    hasil = (int (number1) + int (number2))
+    print (hasil)
+# 4. if user want subtraction,do subtraction
+if option == "2":
+    hasil = (int (number1) - int (number2))
+    print (hasil)
+# 5. if user want multiplication,do multiplication
+if option == "3":
+    hasil = (int (number1) * int (number2))
+    print (hasil)
+# 6. if user want division,do division
+if option == "4":
+    hasil = (int (number1) / int (number2))
+    print (hasil)
+# 7. The user enters option
+# 8. The app show the result
+# 9. The end
--- 01-03-conditionals/readme.md
+++ 01-03-conditionals/readme.md
@@ -3,7 +3,11 @@ This is a code for a simple multi-purpose calculator
 ## Aglorithm
 1. The app shows welcome display
 2. The app asks for user's option
-3. The user enters option
-4. The app show the result
-5. The end
+3. if user want addition,do addition
+4. if user want subtraction,do subtraction
+5. if user want multiplication,do multiplication
+6. if user want division,do division
+7. The user enters option
+8. The app show the result
+9. The end
 
